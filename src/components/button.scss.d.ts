493a19b2d4fc4133d5cf9c9bd79d5da01ef4f8d6
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2023-11-04 00:43:09 +0900

    fix: botton

---
+++ src/components/button.scss.d.ts
@@ -0,0 +1,6 @@
+export declare const storybookButton: string
+export declare const storybookButtonLarge: string
+export declare const storybookButtonMedium: string
+export declare const storybookButtonPrimary: string
+export declare const storybookButtonSecondary: string
+export declare const storybookButtonSmall: string
